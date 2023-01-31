6a02b463537266a23c9f88cb9a8008d733953e1a
todd mccool <toddmccool@gmail.com>
2023-01-30 17:59:30 -0800

    ass again

--- replace.js
+++ replace.js
@@ -6,14 +6,13 @@ document.getElementById("replaceBtn").addEventListener("click", function () {
   text = text.replace(/the/g, "the stupid");
   text = text.replace(/your/g, "that hoe's");
   text = text.replace(/you/g, "them hoes");
+  text = text.replace(/falling in love /g, "eating Ass");
+  text.replace(/Falling in love /g, "eating Ass");
+
+  text = text.replace(/fallin\sin\slove /g, "eatin Ass");
+  text = text.replace(/Fallin\sin\slove /g, "Eatin Ass");
   text = text.replace(/love/g, "ass");
   text = text.replace(/Love/g, "Ass");
-
-  text = text.replace(/falling in love with/g, "eating dat Ass of");
-  text.replace(/Falling in love with/g, "eating dat Ass of");
-
-  text = text.replace(/fallin\sin\slove with/g, "eatin dat Ass of");
-  text = text.replace(/Fallin\sin\slove with/g, "Eatin dat Ass of");
 
   text = text.replace(/\b(a|A)\b/g, "fuckin");
   output.innerHTML = text;
