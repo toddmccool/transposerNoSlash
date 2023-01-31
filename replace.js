302632e78350cd690b0f2782c92e617e3c614704
todd mccool <toddmccool@gmail.com>
2023-01-30 17:42:38 -0800

    eat

--- replace.js
+++ replace.js
@@ -8,6 +8,11 @@ document.getElementById("replaceBtn").addEventListener("click", function () {
   text = text.replace(/you/g, "them hoes");
   text = text.replace(/love/g, "ass");
   text = text.replace(/Love/g, "Ass");
+  text = text.replace(/falling in love with/g, "eating dat Ass of");
+  text = text.replace(/Falling in love with/g, "Eating dat Ass of");
+
+  text = text.replace(/fallin in love with/g, "eatin dat Ass of");
+  text = text.replace(/Fallin in love with/g, "Eatin dat Ass of");
 
   text = text.replace(/\b(a|A)\b/g, "fuckin");
   output.innerHTML = text;
